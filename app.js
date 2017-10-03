402829b90e6565e3bc22ae528e3369ee9942b508
TruongLuan1996 <huuluantruong@gmail.com>
2017-10-03 21:15:18 +0700

    Create

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+    handlebars = require('express-handlebars'),
+    handlebars_sections = require('express-handlebars-sections'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    path = require('path'),
+    wnumb = require('wnumb'),
+    handle404 = require('./middle-wares/handle-404'),
+
+var app = express();
+
+app.use(morgan('dev'));
+
+app.engine('hbs', handlebars({
+    extname: 'hbs',
+    helpers: {
+        section: handlebars_sections(),
+        number_format: function (n) {
+            var nf = wnumb({
+                thousand: ','
+            });
+            return nf.to(n);
+        }
+    }
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(
+    path.resolve(__dirname, 'public')
+));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+app.use(handle404);
+
+app.listen(3000);
